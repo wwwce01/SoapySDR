70eb7aa6e48c3736d330b2309a5f37020eb30db4
Nicholas Corgan <n.corgan@gmail.com>
2021-08-07 15:44:53 -0500

    Added logger test, register with CTest

---
+++ luajit/tests/TestLogger.lua
@@ -0,0 +1,30 @@
+-- Copyright (c) 2021 Nicholas Corgan
+-- SPDX-License-Identifier: BSL-1.0
+
+SoapySDR = require("SoapySDR")
+
+ffi = require("ffi")
+io = require("io")
+luaunit = require("luaunit")
+
+function test_LuaJITLogger()
+    local tmpFile = io.tmpfile()
+
+    function logFunction(logLevel, rawMessage)
+        tmpFile:write(ffi.string(rawMessage), "\n")
+    end
+
+    SoapySDR.Logger.registerHandler(logFunction)
+    SoapySDR.Logger.setLevel(SoapySDR.Logger.Level.INFO)
+
+    SoapySDR.Logger.log(SoapySDR.Logger.Level.WARNING, "Warning")
+    SoapySDR.Logger.log(SoapySDR.Logger.Level.ERROR, "Error")
+
+    tmpFile:seek("set")
+    local logContents = tmpFile:read("*a")
+
+    luaunit.assertEquals(logContents, "Warning\nError\n")
+end
+
+local runner = luaunit.LuaUnit.new()
+os.exit(runner:runSuite())
