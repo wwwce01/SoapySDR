99e02c2d3034f36d73334a0a73b2e4d5f7a4cd47
Nicholas Corgan <n.corgan@gmail.com>
2021-10-23 11:29:18 -0500

    Added Device.Enumerate unit test

---
+++ csharp/tests/TestEnumerateDevices.cs
@@ -0,0 +1,41 @@
+// Copyright (c) 2021 Nicholas Corgan
+// SPDX-License-Identifier: BSL-1.0
+
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class TestSoapyTypes
+{
+    static private bool HasNullDevice(List<Dictionary<string, string>> devices)
+        => devices.Select(args => args.ContainsKey("driver") && (args["driver"] == "null")).Any();
+
+    [Test]
+    public void Test_EnumerateNoParam()
+    {
+        // We can't guarantee the number of devices connected to the machine, so just
+        // make sure this doesn't error out.
+        _ = SoapySDR.Device.Enumerate();
+    }
+
+    [Test]
+    public void Test_EnumerateStringParam()
+    {
+        var args = "driver=null";
+        Assert.IsTrue(HasNullDevice(SoapySDR.Device.Enumerate(args)));
+    }
+
+    [Test]
+    public void Test_EnumerateDictParam()
+    {
+        // Arbitrarily use some non-standard IDictionary subclass to test the
+        // interface parameter.
+        dynamic args = new ExpandoObject();
+        args.driver = "null";
+
+        Assert.IsTrue(HasNullDevice(SoapySDR.Device.Enumerate(args)));
+    }
+}
